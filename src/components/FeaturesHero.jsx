a2255fc093864b189dad20eb8d36fb62ebea1306
Khawaja Hassaan Tariq <khawaja.tariq@transviti.com>
2026-06-18 14:11:32 +0500

    Update

--- src/components/FeaturesHero.jsx
+++ src/components/FeaturesHero.jsx
@@ -2,22 +2,21 @@ import { Plasma } from './Plasma';
 
 export default function FeaturesHero() {
 
-    return (
+  return (
 
-        <section className="features-hero relative overflow-hidden z-0">
-              {/* Plasma Background */}
-              <div className="absolute inset-0 w-full h-full -z-10 pointer-events-none">
-                <Plasma color="#1967e7" speed={1.5} scale={1.3} />
-              </div>
-              
-              <div className="flex flex-col items-center justify-center w-full px-4">
-                <h1 className="hero-title relative z-10 !text-center">Supercharge your workflow</h1>
-                <p className="hero-subtitle !text-center !mx-auto mb-12 relative z-10">
-                    Packed with everything you need to build stunning, high-performance user interfaces in record time.
-                </p>
-              </div>
-        </section>
+    <section className="features-hero relative overflow-hidden z-0">
+      <div className="absolute inset-0 w-full h-full -z-10 pointer-events-none">
+        <Plasma color="#1967e7" speed={1.5} scale={1.3} />
+      </div>
 
-    );
+      <div className="flex flex-col items-center justify-center w-full px-4">
+        <h1 className="hero-title relative z-10 !text-center">Supercharge your workflow</h1>
+        <p className="hero-subtitle !text-center !mx-auto mb-12 relative z-10">
+          Packed with everything you need to build stunning, high-performance user interfaces in record time.
+        </p>
+      </div>
+    </section>
+
+  );
 
 }
